6d5274dd3509eb385352a02506de8981c84325b8
tomo-john <keririnkery.kery@gmail.com>
2026-01-06 08:27:32 +0900

    🐶 chore: fluxを試してみる

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,19 @@
-こんにちわん🐶
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+    <head>
+        @include('partials.head')
+    </head>
+    <body class="min-h-screen bg-white dark:bg-zinc-800">
+        @auth
+            <div class="text-sm text-gray-500">
+                <p>{{ Auth::user()->name }}さんこんにちわん🐶</p>
+            </div>
+        @endauth
+
+        <p>わーん🐶</p>
+
+        <a href="{{ route('home') }}">
+          <flux:button>Home</flux:button>
+        </a>
+    </body>
+</html>
