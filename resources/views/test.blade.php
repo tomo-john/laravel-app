9dc3e874825a7402f391e2d33265f166630b28a2
tomo-john <keririnkery.kery@gmail.com>
2026-01-07 06:56:44 +0900

    🐶 chore: flux: modelの閉じ方

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -8,12 +8,8 @@
 
             {{-- ヘッダー --}}
             <div>
-                <flux:heading size="xl" level="1">
-                    Flux Playground
-                </flux:heading>
-                <flux:subheading>
-                    Fluxの基本を試すテストページ
-                </flux:subheading>
+                <flux:heading size="xl" level="1">Flux Playground</flux:heading>
+                <flux:subheading>Fluxの基本を試すテストページ</flux:subheading>
             </div>
 
             <flux:separator variant="subtle" />
@@ -24,29 +20,19 @@
                 {{-- Left --}}
                 <div class="rounded-xl border border-zinc-200 dark:border-zinc-700 p-6 bg-white dark:bg-zinc-800 space-y-4">
                     <flux:heading size="lg">Left</flux:heading>
-                    <p class="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-                        Button
-                    </p>
-                    <flux:button>
-                        default
-                    </flux:button>
-                    
-                    <flux:button variant="primary">
-                        primary
-                    </flux:button>
 
-                    <flux:button variant="danger">
-                        danger
-                    </flux:button>
+                    <p class="mt-2 text-sm text-zinc-600 dark:text-zinc-400">Button</p>
+
+                    <flux:button>default</flux:button>
+                    <flux:button variant="primary">primary</flux:button>
+                    <flux:button variant="danger">danger</flux:button>
                 </div>
 
                 {{-- Right --}}
                 <div class="rounded-xl border border-zinc-200 dark:border-zinc-700 p-6 bg-white dark:bg-zinc-800 space-y-4">
                     <flux:heading size="lg">Right</flux:heading>
 
-                    <p class="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-                        モーダル
-                    </p>
+                    <p class="mt-2 text-sm text-zinc-600 dark:text-zinc-400">モーダル</p>
 
                     <flux:modal.trigger name="test-modal">
                         <flux:button variant="primary">
@@ -59,15 +45,8 @@
             {{-- Modal --}}
             <flux:modal name="test-modal" title="Flux Modal">
                 <div class="space-y-4">
-                    <p class="text-sm">
-                        これはFluxのモーダルです。
-                    </p>
-
-                    <div class="flex justify-end">
-                        <flux:button x-on:click="$modal.close('test-modal')">
-                            閉じる
-                        </flux:button>
-                    </div>
+                    <p class="text-sm">これはFluxのモーダルです。</p>
+                    <flux:modal.close><flux:button>close</flux:button></flux:modal.close>
                 </div>
             </flux:modal>
 
