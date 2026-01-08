dd036074d766bc350aa8d55907268fd24eac0410
tomo-john <keririnkery.kery@gmail.com>
2026-01-08 16:12:16 +0900

    🐰 refactor: Eagerロード

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::with('user')->get();
         return view('post.index', compact('posts'));
     }
 
