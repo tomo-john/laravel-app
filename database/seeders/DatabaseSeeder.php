fc75c932f59d5f193d837eaf13318f4b31cd0a51
tomo-john <keririnkery.kery@gmail.com>
2026-01-14 07:15:07 +0900

    🐶 test: Factory試してみる

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,16 +8,15 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //    'name' => 'Test User',
+        //    'email' => 'test@example.com',
+        // ]);
+
+        \App\Models\Post::factory(3)->create();
     }
 }
